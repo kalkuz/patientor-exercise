0554a06a111b9a310c657c92a7c9dfdadba959d1
Oğuzhan Kalkuz <kalkuzoguzhann@gmail.com>
2022-11-01 16:05:18 +0300

    9.22

--- src/PatientDetailsPage/index.tsx
+++ src/PatientDetailsPage/index.tsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { apiBaseUrl } from "../constants";
-import { Patient } from "../types";
+import { Diagnosis, Patient } from "../types";
 import { useParams } from "react-router-dom";
 
 const PatientListPage = () => {
   const { id } = useParams<{ id: string }>();
 
   const [patient, setPatient] = useState<Patient>();
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>();
 
   const GetPatient = async () => { 
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
@@ -15,7 +16,13 @@ const PatientListPage = () => {
     setPatient(response.data);
   };
 
-  useEffect(() => { void GetPatient(); }, []);
+  const GetDiagnoses = async () => { 
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    const response = await axios.get<Diagnosis[]>(`${apiBaseUrl}/diagnoses`);
+    setDiagnoses(response.data);
+  };
+
+  useEffect(() => { void GetPatient(); void GetDiagnoses(); }, []);
 
   return (
     <div className="App">
@@ -26,6 +33,25 @@ const PatientListPage = () => {
           </h1>
           <p>ssn: {patient.ssn}</p>
           <p>{patient.occupation}</p>
+          
+          <h2>entries</h2>
+          {patient.entries.map((e) => (
+            <div 
+              key={e.id}
+              style={{ border: '1px solid black', borderRadius: '1rem', padding: "1rem", marginBlock: '1rem' }}
+            >
+              <p>{e.date}</p>
+              <p><i>{e.description}</i></p>
+              <div>
+                {e.diagnosisCodes?.map((dc) => (
+                  <div 
+                    key={`${e.id} ${dc}`}
+                  >{dc} {diagnoses?.find((dg) => dg.code === dc)?.name}</div>
+                ))}
+              </div>
+              <p>Diagnose By: {e.specialist}</p>
+            </div>
+          ))}
         </>
       )}
     </div>
--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,50 @@ export interface Diagnosis {
   latin?: string;
 }
 
+interface BaseEntry {
+  id: string;
+  type: string;
+  date: string;
+  specialist: string;
+  description: string;
+  diagnosisCodes?: Array<string>;
+  discharge?: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry 
+  | HealthCheckEntry 
+;
+
 export enum Gender {
   Male = "male",
   Female = "female",
@@ -13,8 +57,9 @@ export enum Gender {
 export interface Patient {
   id: string;
   name: string;
+  ssn: string;
   occupation: string;
   gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+  dateOfBirth: string;
+  entries: Entry[]
 }
